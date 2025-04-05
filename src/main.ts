c510f37861ad4a6817c742bfc449006c448efd54
MrAmericanMike <mramericanmike@gmail.com>
2025-04-05 10:22:55 -0300

    Unbuild testing

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,6 @@
 import { CompassPoints, FormatType, WindDirectionRange } from "./types";
 
-class WindCompass {
+export default class WindCompass {
 	private static DIRECTIONS: WindDirectionRange[] = [
 		{ range: [0, 11.25], point: "N" },
 		{ range: [11.25, 33.75], point: "NNE" },
@@ -144,5 +144,3 @@ class WindCompass {
 		}
 	}
 }
-
-export default WindCompass;
